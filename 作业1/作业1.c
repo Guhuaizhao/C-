2e0d24794c7f1c2b93ac016361d04afb08ed07c6
Guhuaizhao <1579569222@qq.com>
2018-04-09 21:23:33 +0800

    C语言作业
    C语言作业

---
+++ 作业1/作业1.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#define PI 3.1415926
+void main()
+{
+	float r;
+	float area;
+	printf("ÇëÊäÈëÔ²µÄ°ë¾¶\n");
+	scanf("%f",&r);
+	area=PI*r*r;
+	printf("area=%f\n",area);
+}
---
+++ 作业2/作业2.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+void main()
+{
+	int a;
+	a=500;
+	printf("\"How many student here?\"\n\"%d\"",a);
+}
